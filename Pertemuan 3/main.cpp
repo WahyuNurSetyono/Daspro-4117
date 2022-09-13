845839add59bdae985f330d5dbaaac2df37d61db
Wahyu Nur Setyono <111202214633@mhs.dinus.ac.id>
2022-09-13 17:35:50 +0700

    Pertemuan 3

---
+++ Pertemuan 3/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <istream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+    string a;
+    char huruf;
+    int k = 4117;
+    cout << "Wellcome!" << endl;
+    cout << "I'm not robot           : "; cin>> a;
+    printf("Huruf Pertama Nama Anda : ");
+    scanf(" %c",&huruf);
+    cout << "\nNama           : Wahyu Nur Setyono \nNIM            : A11.2022.14633 \nKelp.          : " << k;
+    //* coment
+    //
+
+    int t = 171.5;
+    char g = 'O';
+    string j = "Laki-Laki";
+
+
+
+    cout << "\nTinggi Badan   : " << t;
+    cout << "\nGolongan Darah : " << g;
+    cout << "\nJenis Kelamin  : " << j;
+    return 0;
+}
